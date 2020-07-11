35db840cfbacfee85790be683c32fd403aa08818
mdctleo <mdctleo@hotmail.com>
2020-07-11 15:45:04 -0700

    deployment with amazon working somewhat

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ export let BASE_API = ""
 if (process.env.NODE_ENV != 'production') {
     BASE_API = "http://127.0.0.1:5000"
 } else {
-    BASE_API = ""
+    BASE_API = "/api"
 }
 
 ReactDOM.render(
